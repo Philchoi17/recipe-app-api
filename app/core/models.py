1b953f0a7f38a256bd26939c61ad916b59ebb8c8
PhilChoi <philchoi@icloud.com>
2020-04-22 16:59:44 +0900

    fixed some stuff for flake8

--- app/core/models.py
+++ app/core/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
                                         PermissionsMixin
 
+
 class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,8 +1,9 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+
 class ModelTests(TestCase):
-    
+
     def test_create_user_with_email_successful(self):
         """Test creating a new user with an email is successful"""
         email = 'test@philthylove.com'
